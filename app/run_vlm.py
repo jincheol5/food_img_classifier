241420661368360bb6c7f3b452de6d725c8e2f45
jincheol5 <oscar4293@gmail.com>
2026-04-07 02:44:19 +0900

    update

--- app/run_vlm.py
+++ app/run_vlm.py
@@ -8,43 +8,105 @@ def main(app_config:dict):
         case 1:
             """
             App 1.
-            Load and save pretrained LLM and its tokenizer
+            Load and save pretrained LLM and its tokenizer or processor
                 Qwen3.5
             """
             model_config={
                 "torch_dtype":torch.bfloat16,
                 "device_map":"auto"
             }
-            ModelUtils.save_pretrained_causal_llm_from_HF(
+            ModelUtils.save_pretrained_llm_from_HF(
                 HF_path=app_config["HF_path"],
                 model_name=app_config["model_name"],
+                model_type=app_config["model_type"]
                 **model_config
             )
-            ModelUtils.save_tokenizer_from_HF(
-                HF_path=app_config["HF_path"],
-                model_name=app_config["model_name"]
-            )
+            match app_config["model_type"]:
+                case "llm":
+                    ModelUtils.save_tokenizer_from_HF(
+                        HF_path=app_config["HF_path"],
+                        model_name=app_config["model_name"]
+                    )
+                case "vlm":
+                    ModelUtils.save_processor_from_HF(
+                        HF_path=app_config["HF_path"],
+                        model_name=app_config["model_name"]
+                    )
 
         case 2:
             """
             App 2.
-
+            OCR 
             """
+            model=ModelUtils.load_local_llm(model_name=app_config["model_name"],model_type=app_config["model_type"])
+            processor=ModelUtils.load_local_processor(model_name=app_config["model_name"])
 
+            image_paths=[
+                os.path.join("dataset", "images", "2087686040757.png"), # 원재료
+                os.path.join("dataset", "images", "8800279679073.png"), # 영양성분
+                os.path.join("dataset", "images", "0000088002798.png"), # 식품사진
+            ]
 
+            messages=[
+                {
+                    "role":"system",
+                    "content":[
+                        {
+                            "type":"text",
+                            "text":f"You are an OCR system. Extract all visible text from the given image exactly as it appears."
+                        }
+                    ] 
+                },
+                {
+                    "role":"user",
+                    "content":[
+                        {
+                            "type":"image",
+                            "url":image_paths[1]
+                        },
+                        {
+                            "type":"text",
+                            "text":f"Extract raw text."
+                        }
+                    ] 
+                }
+            ]
 
+            inputs=processor.apply_chat_template(
+                messages,
+                add_generation_prompt=True,
+                tokenize=True,
+                return_dict=True,
+                return_tensors="pt",
+            ).to(model.device)
 
+            with torch.no_grad():
+                outputs=model.generate(
+                    **inputs,
+                    max_new_tokens=512,
+                    do_sample=False,   # OCR은 deterministic이 좋음
+                )
+            
+            # 입력 길이 이후만 잘라서 디코딩
+            generated_ids=outputs[0][inputs["input_ids"].shape[-1]:]
+            result=processor.decode(
+                generated_ids,
+                skip_special_tokens=True
+            )
+            print(result)
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("--app_num",type=int,default=1)
     parser.add_argument("--HF_path",type=str,default="Qwen/Qwen3.5-9B") 
     parser.add_argument("--model_name",type=str,default="Qwen3.5-9B") 
+    parser.add_argument("--model_type",type=str,default="llm") 
     args=parser.parse_args()
     app_config={
         # app 관련
         "app_num":args.app_num,
         "HF_path":args.HF_path,
-        "model_name":args.model_name
+        "model_name":args.model_name,
+        "model_type":args.model_type
     }
     main(app_config=app_config)
--- modules/model_utils.py
+++ modules/model_utils.py
@@ -1,6 +1,7 @@
 import os
 import torch
-from transformers import AutoTokenizer,AutoModelForCausalLM,AutoModelForImageTextToText
+from typing_extensions import Literal
+from transformers import AutoTokenizer,AutoProcessor,AutoModelForCausalLM,AutoModelForImageTextToText
 from transformers import LogitsProcessor
 
 class RestrictTokensProcessor(LogitsProcessor):
@@ -18,29 +19,30 @@ class ModelUtils:
     dir_path=os.path.join("..","data","llm")
 
     @staticmethod
-    def save_pretrained_causal_llm_from_HF(HF_path:str,model_name:str,**kwargs):
+    def save_pretrained_llm_from_HF(HF_path:str,model_name:str,model_type:Literal["llm","vlm"]=f"llm",**kwargs):
         """
-        model_config
+        **kwargs
             torch_dtype: auto or torch.bfloat16 (GPU)
             device_map: "auto", weight를 읽는 순간 바로 GPU/CPU에 분산 배치
         """
         model_path=os.path.join(ModelUtils.dir_path,"pretrained",model_name)
         os.makedirs(model_path,exist_ok=True) # 해당 경로의 모든 폴더 없으면 생성
 
-        model=AutoModelForCausalLM.from_pretrained(
-            pretrained_model_name_or_path=HF_path,
-            torch_dtype=kwargs["torch_dtype"] ,
-            device_map=kwargs["device_map"] 
-        )
+        match model_type:
+            case "llm":
+                model=AutoModelForCausalLM.from_pretrained(
+                    pretrained_model_name_or_path=HF_path,
+                    torch_dtype=kwargs["torch_dtype"] ,
+                    device_map=kwargs["device_map"] 
+                )
+            case "vlm":
+                model=AutoModelForImageTextToText.from_pretrained(
+                    pretrained_model_name_or_path=HF_path,
+                    torch_dtype=kwargs["torch_dtype"] ,
+                    device_map=kwargs["device_map"] 
+                )
         model.save_pretrained(model_path)
-        print(f"Save pretrained causal llm: {model_name} from {HF_path}!")
-
-    @staticmethod
-    def save_pretrained_vlm_from_HF(HF_path:str,model_name:str,**kwargs):
-        """
-        """
-        model_path=os.path.join(ModelUtils.dir_path,"pretrained",model_name)
-        os.makedirs(model_path,exist_ok=True) # 해당 경로의 모든 폴더 없으면 생성
+        print(f"Save pretrained {model_type}: {model_name} from {HF_path}!")
 
     @staticmethod
     def save_tokenizer_from_HF(HF_path:str,model_name:str):
@@ -55,13 +57,29 @@ class ModelUtils:
         print(f"Save tokenizer for pretrained causal llm: {model_name} from {HF_path}!")
     
     @staticmethod
-    def load_local_causal_llm(model_name:str):
+    def save_processor_from_HF(HF_path:str,model_name:str):
+        processor_path=os.path.join(ModelUtils.dir_path,"pretrained",model_name)
+        os.makedirs(processor_path,exist_ok=True) # 해당 경로의 모든 폴더 없으면 생성
+        tokenizer=AutoProcessor.from_pretrained(
+            pretrained_model_name_or_path=HF_path
+        )
+        tokenizer.save_pretrained(processor_path)
+        print(f"Save processor for pretrained vlm: {model_name} from {HF_path}!")
+    
+    @staticmethod
+    def load_local_llm(model_name:str,model_type:Literal["llm","vlm"]=f"llm"):
         """
         """
         model_path=os.path.join(ModelUtils.dir_path,"pretrained",model_name)
-        model=AutoModelForCausalLM.from_pretrained(
-            model_path
-        )
+        match model_type:
+            case "llm":
+                model=AutoModelForCausalLM.from_pretrained(
+                    model_path
+                )
+            case "vlm":
+                model=AutoModelForImageTextToText.from_pretrained(
+                    model_path
+                )
         return model
 
     @staticmethod
@@ -70,6 +88,16 @@ class ModelUtils:
         """
         model_path=os.path.join(ModelUtils.dir_path,"pretrained",model_name)
         tokenizer=AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=model_path
+        )
+        return tokenizer
+    
+    @staticmethod
+    def load_local_processor(model_name:str):
+        """
+        """
+        model_path=os.path.join(ModelUtils.dir_path,"pretrained",model_name)
+        tokenizer=AutoProcessor.from_pretrained(
             pretrained_model_name_or_path=model_path
         )
         return tokenizer
